9f6026ff9699ce17cc759f61e570d36972bfa2d3
Augs Machado <augustomachadoramos@gmail.com>
2021-04-28 08:10:07 -0300

    Setting project

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep: src/main.rs
+
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/minigrep: /Users/augustomachado/github/rust_grep_cli/minigrep/src/main.rs
