15c1f9000d7e7b500b81c83a152b2323999e7ad2
Augs Machado <augustomachadoramos@gmail.com>
2021-04-28 08:49:57 -0300

    [src/main.rs] Extacting the argument parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,10 @@ fn main() {
     // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection
     // such as a vector, containing all the elements the iterator produces.
     let args: Vec<String> = env::args().collect();
-    
-    // Saves the values of the two arguments in variables so we can use the values throughout the rest of the program.
-    // When we printed the vector, the program's name takes up the first value in the vector at args[0], so we're starting at the
-    // index 1. 
-    let query = &args[1];
-    let filename = &args[2];
 
+    // Saves the values of the two arguments in variables so we can use the values throughout the rest of the program.
+    let (query, filename) = parse_config(&args);
+    
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
@@ -19,4 +16,13 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents);
 
+}
+
+fn parse_config (args: &[String]) -> (&str, &str) {
+    // When we printed the vector, the program's name takes up the first value in the vector at args[0], so we're starting at the
+    // index 1. 
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
