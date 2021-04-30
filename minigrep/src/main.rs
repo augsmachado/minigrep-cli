09d0c28d69f89570ea5d358f211fabde4a8cc4d5
Augs Machado <augustomachadoramos@gmail.com>
2021-04-30 08:07:52 -0300

    [src/main.rs] Creating a constructor for config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Saves the values of the two arguments in variables so we can use the values throughout the rest of the program.
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -23,11 +23,15 @@ struct Config {
     filename: String,
 }
 
-fn parse_config (args: &[String]) -> Config {
-    // When we printed the vector, the program's name takes up the first value in the vector at args[0], so we're starting at the
-    // index 1. 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+// Note: using primitive values when a complex type would be more appropriate is an anti-pattern knwon as primitive obsession.
+impl Config {
+    fn new(args: &[String]) -> Config {
+        
+        // When we printed the vector, the program's name takes up the first value in the vector at args[0], so we're starting at the
+        // index 1. 
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
