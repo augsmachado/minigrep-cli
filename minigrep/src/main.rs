039f34d6837e355125c0e6ce6b58666ffd342ca7
Augs Machado <augustomachadoramos@gmail.com>
2021-05-01 12:22:46 -0300

    [src/main.rs] exiting with an error code if creating a new Config fails

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection
@@ -7,7 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Saves the values of the two arguments in variables so we can use the values throughout the rest of the program.
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // A nonzero exit status is a convention to signal to the process that called our program that the program exited with an
+        // error state
+        println!("Problems parsing arguments: {}", err);
+        process::exit(1);
+    });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
