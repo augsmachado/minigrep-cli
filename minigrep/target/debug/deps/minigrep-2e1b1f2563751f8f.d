5fe1b60e7f87125680616ab40ac455db4e6f7625
Augs Machado <augustomachadoramos@gmail.com>
2021-05-21 23:45:24 -0300

    .

---
+++ minigrep/target/debug/deps/minigrep-2e1b1f2563751f8f.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep-2e1b1f2563751f8f.rmeta: src/lib.rs
+
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep-2e1b1f2563751f8f.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-b04020a86a73c96f.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep-b04020a86a73c96f.rmeta: src/main.rs
+
+/Users/augustomachado/github/rust_grep_cli/minigrep/target/debug/deps/minigrep-b04020a86a73c96f.d: src/main.rs
+
+src/main.rs:
